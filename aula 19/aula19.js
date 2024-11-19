7ffe15cc81e767e60154518130189529651dbd80
Luana <luana.lisboav@gmail.com>
2024-11-19 20:38:57 -0300

    .

--- aula 19/aula19.js
+++ aula 19/aula19.js
@@ -10,16 +10,30 @@ function moveCarousel() {
     // Incrementa o índice para avançar uma imagem
     index++;
 
-    // Verifica se o índice chegou à última imagem
+    // Verifica se o índice chegou à última imagem 
     if (index >= items.length - 1) {
         // Pequeno atraso para permitir que a transição termine
-        setTimeout(() => {
+        ///define um atraso de 500 milisegundos antes de executar a função de retorno ao iniciar
+        setTimeout(() => {  //setTimeou: está trabalhando com o tema de alguma coisa
+            //remove a transição para que o retorno...    
             track.style.transition = 'none'; // Remove a animação
+
+            //reseta o indice para 0, que representa a primeira imagem
             index = 0; // Volta ao início (primeira imagem)
+
+            //move o carrossel de volta para a primeira imagem (poisição inicial)
             track.style.transform = `translateX(0)`; // Move para a primeira imagem
+
         }, 500); // Tempo suficiente para completar a transição visível
-    } else { 
+    } else {
+
+        //caso o indice ainda não tenha atingido o final, move o carrossel para a primeira imagem
+
+        //adiciona uma transição suave de 0.3 segundos para o movimento do carrossel
+
         // Move o carrossel para a próxima imagem
+        //move o carrossel para a esquerda, usando o indice geral para calcular o deslocamento
+        //ex: se index = 1, desloca -100%, se index = 2, desloca -200%, etc.
         track.style.transition = 'transform 0.5s ease'; // Adiciona a animação
         track.style.transform = `translateX(-${index * 100}%)`; // Desloca o carrossel
     }
